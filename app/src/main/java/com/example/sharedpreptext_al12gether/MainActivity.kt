c4ab7aeaddcde751caebef0b097ca20a1e9a212b
al12gether <alonetogetherpd@gmail.com>
2022-04-20 15:40:37 +0900

    ContextUtil 추가 -> 자동로그인 저장 기능  - 저장된 자동로그인 여부 확인 작업 필요

--- app/src/main/java/com/example/sharedpreptext_al12gether/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreptext_al12gether/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.sharedpreptext_al12gether
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sharedpreptext_al12gether.utils.ContextUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
